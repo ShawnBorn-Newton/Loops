c0921060d43387290ee4d71954a10affe07d2486
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-06 10:12:37 -0400

    practise1

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,30 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            string[] days = { "Monday", "Tuesday", "Wendsday" };
+
+            for (int i = days[0]; i > days.Length; i++)
+            {
+                {
+                    Console.WriteLine(i);
+                }//end if
+
+            }//end for
+
+            //for(int i = 0; i <=10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 15; i > 0; i--) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 50; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
         }
     }
 }
